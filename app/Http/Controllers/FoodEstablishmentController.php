4e572df8bc80251d36134752d662ee6e167fe9cd
kiano-serha <kian.oconnor.serha@gmail.com>
2024-03-15 09:00:50 -0500

    food establishment table

--- app/Http/Controllers/FoodEstablishmentController.php
+++ app/Http/Controllers/FoodEstablishmentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\EstablishmentApplications;
 use App\Models\FoodEstablishment;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -12,7 +13,9 @@ class FoodEstablishmentController extends Controller
 {
     public function index()
     {
-        return view('establishments.index');
+        $food_establishments = EstablishmentApplications::where('created_at', '>', "2024-01-01")->get();
+
+        return view('establishments.index', compact('food_establishments'));
     }
 
     public function view()
@@ -20,33 +23,34 @@ class FoodEstablishmentController extends Controller
         //Fetch all certificates 
 
         $certificate = FoodEstablishment::join('food_est_operators', 'food_est_operators.establishment_application_id', '=', 'establishment_applications.id')
-        ->join('establishment_categories', 'establishment_categories.id', '=', 'establishment_applications.establishment_category_id')
-        ->leftJoin('sign_offs', function ($join) {
-            $join->on('sign_offs.application_id', '=', 'establishment_applications.id')
-                ->where('application_type_id', '=', 3);
-        })
-        ->join('users', 'users.id', '=', 'establishment_applications.user_id')
-        ->leftJoin('payments', function ($join) {
-            $join->on('payments.application_id', '=', 'establishment_applications.id')
-                ->where('payments.deleted_at','=',null)
-                ->where('payments.application_type_id', '=', 3)
-                ->where('payments.facility_id', '=', auth()->user()->facility_id);
-        })
-        ->select('establishment_applications.*', 
-            'food_est_operators.name_of_operator as operators',
-            'establishment_categories.name as categories', 
-            'payments.created_at as payment',
-            'sign_offs.expiry_date',
-            'users.firstname',
-            'users.lastname'
-        )
-        ->whereIn('establishment_applications.user_id', User::facilityUsers()->pluck('id')->flatten())
-        ->where('establishment_applications.deleted_at','=',null)
-        //->groupBy('establishment_applications.id')
-        ->orderBy('establishment_applications.application_date', 'desc')
-        ->distinct('establishment_applications.id')
-        ->get();
-    
+            ->join('establishment_categories', 'establishment_categories.id', '=', 'establishment_applications.establishment_category_id')
+            ->leftJoin('sign_offs', function ($join) {
+                $join->on('sign_offs.application_id', '=', 'establishment_applications.id')
+                    ->where('application_type_id', '=', 3);
+            })
+            ->join('users', 'users.id', '=', 'establishment_applications.user_id')
+            ->leftJoin('payments', function ($join) {
+                $join->on('payments.application_id', '=', 'establishment_applications.id')
+                    ->where('payments.deleted_at', '=', null)
+                    ->where('payments.application_type_id', '=', 3)
+                    ->where('payments.facility_id', '=', auth()->user()->facility_id);
+            })
+            ->select(
+                'establishment_applications.*',
+                'food_est_operators.name_of_operator as operators',
+                'establishment_categories.name as categories',
+                'payments.created_at as payment',
+                'sign_offs.expiry_date',
+                'users.firstname',
+                'users.lastname'
+            )
+            ->whereIn('establishment_applications.user_id', User::facilityUsers()->pluck('id')->flatten())
+            ->where('establishment_applications.deleted_at', '=', null)
+            //->groupBy('establishment_applications.id')
+            ->orderBy('establishment_applications.application_date', 'desc')
+            ->distinct('establishment_applications.id')
+            ->get();
+
         //dd($certificate);
 
         return view('establishments.view', compact('certificate'));
