754d9b9445d94daaee1d3e7674210a94034faa64
tywayneb <tywayneb@serha.gov.jm>
2024-04-02 13:00:58 -0500

    made changes to table

--- resources/views/partials/tables/currentUsers.blade.php
+++ resources/views/partials/tables/currentUsers.blade.php
@@ -41,11 +41,16 @@
 
 
 <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
-<script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.5.0/js/responsive.bootstrap5.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
+
 
  <script>
-     new DataTable('#users', {
+     new DataTable('#currentUsers', {
          responsive: true,
          scrollX:true,
      });
