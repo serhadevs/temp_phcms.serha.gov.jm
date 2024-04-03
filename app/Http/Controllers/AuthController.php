cdcbfdeb45307b429221b618a6c205136ba0632b
tywayneb <tywayneb@serha.gov.jm>
2024-04-03 08:39:51 -0500

    undo changes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -47,32 +47,35 @@ class AuthController extends Controller
                 return redirect('/change-password')->with('info', 'You are required to change your password.');
             }
 
+            //Check to see if they are logged in already by checking the session_id in the
+            //loginlocation table
+
+           
+
             $user = Auth::user();
             $request->session()->regenerate();
             Auth::login($user, $request->get('remember_token'));
             
             //Generate the session id
 
-            // $session_id = rand(10000000,9999999);
-
             $session_id = Str::random(20);
             //Store it in a session 
 
             session(['session_id' => $session_id]);
 
-            $location = LoginActivity::create([
-                'login_time' => now(),
-                'user_id' => Auth::user()->id,
-                'facility_id' => Auth::user()->facility_id,
-                'user_agent' => $userAgent,
-                'platform' => $userPlatform,
-                'session_id' => $session_id,
-                'ip_address' => $request->ip()
-            ]);
+            // $location = LoginActivity::create([
+            //     'login_time' => now(),
+            //     'user_id' => Auth::user()->id,
+            //     'facility_id' => Auth::user()->facility_id,
+            //     'user_agent' => $userAgent,
+            //     'platform' => $userPlatform,
+            //     'session_id' => $session_id,
+            //     'ip_address' => $request->ip()
+            // ]);
 
-            if (!$location) {
-                return redirect('/')->with('error', 'Unable to store login information');
-            }
+            // if (!$location) {
+            //     return redirect('/')->with('error', 'Unable to store login information');
+            // }
 
             return redirect()->intended('/dashboard')->with('success', 'User logged in successfully!');
         } catch (\Exception $e) {
@@ -88,18 +91,18 @@ class AuthController extends Controller
     {
         // $userId = Auth::id();
          //Find the user in the database that matches the id
-         $sessionId = Session::get('session_id');
+        //  $sessionId = Session::get('session_id');
 
-         //dd($sessionId);
-         $location = LoginActivity::where('session_id',$sessionId)->first();
+        //  //dd($sessionId);
+        //  $location = LoginActivity::where('session_id',$sessionId)->first();
  
-         //dd($location);
+        //  //dd($location);
  
-         if ($location) {
-             $location->update([
-                 'logout_time' => now()
-             ]);
-         }
+        //  if ($location) {
+        //      $location->update([
+        //          'logout_time' => now()
+        //      ]);
+        //  }
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
