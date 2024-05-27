49062862b596104a6f4ef7b1ab71c397612f72ab
kiano-serha <kian.oconnor.serha@gmail.com>
2024-05-27 13:30:05 -0500

    fix food handlers table fix

--- resources/views/partials/tables/food_handlers_permits_table.blade.php
+++ resources/views/partials/tables/food_handlers_permits_table.blade.php
@@ -109,13 +109,13 @@
 <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.bootstrap5.min.css">
 
 {{-- Buttons links --}}
-<script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
+{{-- <script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
 <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.print.min.js"></script>
-<script src="https://cdn.datatables.net/plug-ins/1.13.7/api/sum().js"></script>
+<script src="https://cdn.datatables.net/plug-ins/1.13.7/api/sum().js"></script> --}}
 
 <script>
     var table = new DataTable('#food_handlers_permit', {
@@ -145,11 +145,11 @@
         scrollX: true
     });
 
-    table.buttons = [
-        'copy', 'csv', 'excel', 'pdf', 'print'
-    ];
+    // table.buttons = [
+    //     'copy', 'csv', 'excel', 'pdf', 'print'
+    // ];
 </script>
-<script>
+{{-- <script>
     window.onload = () => {
         buttons = document.querySelectorAll("div.dt-buttons button");
 
@@ -159,5 +159,5 @@
             element.classList.add("btn-secondary")
         })
     }
-</script>
+</script> --}}
 @include('partials.messages.remove_entry_message')
