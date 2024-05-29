ba1a8c167d717c1455589664b030df7b65acfed7
Tywayne Buchanan <tywaynebuchanan@gmail.com>
2024-05-29 06:36:59 -0500

    chore: fixed issue with tourist est link

--- resources/views/partials/sidebar/_sidebar.blade.php
+++ resources/views/partials/sidebar/_sidebar.blade.php
@@ -243,7 +243,7 @@
                         <a href="/downloads/food-establishments/filter/0" class="sidebar-link">Food Establishments</a>
                     </li>
                     <li class="sidebar-item">
-                        <a href="/downloads/food-establishments/filter/0" class="sidebar-link">Tourist
+                        <a href="/downloads/tourist-establishments/filter/0" class="sidebar-link">Tourist
                             Establishments</a>
                     </li>
                 </ul>
