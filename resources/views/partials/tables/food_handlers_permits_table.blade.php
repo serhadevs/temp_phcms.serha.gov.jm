13a5717dd85ce142c712cbd13282ce7e68335ea2
kiano-serha <kian.oconnor.serha@gmail.com>
2024-06-11 15:10:00 -0500

    allow view image in advanced search for food handlers

--- resources/views/partials/tables/food_handlers_permits_table.blade.php
+++ resources/views/partials/tables/food_handlers_permits_table.blade.php
@@ -2,6 +2,9 @@
     <thead>
         {{-- Only shows your facility --}}
         <tr>
+            @if (isset($module))
+                <th></th>
+            @endif
             <th>App #</th>
             <th>Permit No.</th>{{-- THERE --}}
             <th>First Name</th>{{-- THERE --}}
@@ -25,6 +28,17 @@
     <tbody>
         @foreach ($permit_applications as $permit_application)
             <tr>
+                @if (isset($module))
+                    <td>
+                        @if ($permit_application->photo_upload && $permit_application->photo_upload != 0)
+                            <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal"
+                                data-bs-target="#staticBackdrop"
+                                onclick="populateModal({{ json_encode($permit_application->id) }}, {{ json_encode(strtoupper($permit_application->firstname . ' ' . $permit_application->lastname)) }}, {{ json_encode($permit_application->photo_upload) }})">
+                                Photo
+                            </button>
+                        @endif
+                    </td>
+                @endif
                 <td>{{ $permit_application->id }}</td>
                 <td>{{ strtoupper($permit_application->permit_no) }}</td>
                 <td>{{ strtoupper($permit_application->firstname) }}</td>
@@ -77,6 +91,9 @@
     </tbody>
     <tfoot>
         <tr>
+            @if (isset($module))
+                <th></th>
+            @endif
             <th>App #</th>
             <th>Permit No.</th>{{-- THERE --}}
             <th>First Name</th>{{-- THERE --}}
@@ -107,6 +124,27 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.bootstrap5.min.css">
+
+@if (isset($module))
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel"></h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <img alt="No image found" class="w-100 mx-auto rounded" id="modal_image">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close Photo</button>
+                    {{-- <button type="button" class="btn btn-primary">Understood</button> --}}
+                </div>
+            </div>
+        </div>
+    </div>
+@endif
 
 @if (isset($is_general_report))
     {{-- Buttons links --}}
@@ -150,6 +188,13 @@
         });
     </script>
 @else
+    <script>
+        function populateModal(application_id, applicant_name, photo_path) {
+            document.querySelector('.modal-title').innerHTML = application_id + ' - ' + applicant_name;
+            const current_src = "{{ asset('storage/') }}";
+            document.getElementById('modal_image').setAttribute("src", current_src + "/" + photo_path);
+        }
+    </script>
     <script>
         var table = new DataTable('#food_handlers_permit', {
             initComplete: function() {
