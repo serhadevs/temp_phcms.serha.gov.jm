d087ba8b0aaf3db28f90ad278b4a86cf6a8796e8
Tywayne Buchanan <tywaynebuchanan@gmail.com>
2024-06-17 11:55:44 -0500

    adjusted table

--- resources/views/partials/tables/onsite_list_table.blade.php
+++ resources/views/partials/tables/onsite_list_table.blade.php
@@ -1,4 +1,4 @@
-<table class="table table-striped nowarp" id="food_clinics" style="width:100%">
+<table class="table table-bordered table-striped nowrap table-sm" id="food_clinics" style="width:100%">
     <thead>
         <tr>
             <th>App #</th>
@@ -50,6 +50,7 @@
     </tbody>
 </table>
 
+
 <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
@@ -58,39 +59,46 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.bootstrap5.min.css">
-@include('partials.messages.remove_entry_message')
 
-    {{-- Button links --}}
-    <script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-    <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.print.min.js"></script>
-    <script src="https://cdn.datatables.net/plug-ins/1.13.7/api/sum().js"></script>
-    
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            const dataTableElement = document.getElementById('food_clinics');
-    
-            if (dataTableElement) {
-                const dataTable = new DataTable(dataTableElement, {
-                    dom: 'Bfrtip',
-                    buttons: [
-                        'copy', 'csv', 'excel', 'pdf', 'print'
-                    ],
-                    scrollX: true,
-                    responsive: true,
-                    initComplete: function() {
-                        // Assuming `loading` is a function or object that manages a loading indicator
-                        if (typeof loading !== 'undefined') {
-                            loading.close();
-                        }
-                    }
-                });
-            }
-        });
+<script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.print.min.js"></script>
+<script src="https://cdn.datatables.net/plug-ins/1.13.7/api/sum().js"></script>
 
-    </script>
+
+<script>
+    new DataTable('#food_clinics', {
+        scrollX: true,
+        initComplete: function() {
+            loading.close()
+        },
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ],
+        "order": [],
+        "footerCallback": function(row, data, start, end, display) {
+            var api = this.api(),
+                data;
+        },
+        "aoColumnDefs": [{
+            "bSortable": false,
+            "aTargets": ["sorting_disabled"]
+        }],
+    });
+</script>
+
+<script>
+    window.onload = () => {
+        buttons = document.querySelectorAll("div.dt-buttons button");
+        buttons.forEach((element) => {
+            element.classList.add("btn");
+            element.classList.add("btn-secondary")
+        })
+    }
+</script>
 
   
