d2d6043080da2516fec5de4be33f3f255b94e4a8
Tywayne Buchanan <tywaynebuchanan@gmail.com>
2024-06-24 08:06:57 -0500

    made changes

--- resources/views/partials/tables/outstanding_test_results_ests.blade.php
+++ resources/views/partials/tables/outstanding_test_results_ests.blade.php
@@ -1,4 +1,4 @@
-<table class="table table-striped no-warp" id="outstanding_results" style="width:100%">
+<table class="table table-striped nowarp table-bordered" id="outstanding_results" style="width:100%">
     <thead>
         <tr>
             <th>App No.</th>
