905f86998e140b41be3321e1c3d1dda39687af8e
Tywayne Buchanan <tywaynebuchanan@gmail.com>
2024-06-27 09:37:36 -0500

    made changes to dashboard

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -3,26 +3,51 @@
 namespace App\Http\Controllers;
 
 use App\Models\Appointments;
-use App\Models\User;
+use App\Models\BarbershopHairSalons;
+use App\Models\EstablishmentApplications;
+use App\Models\Payments;
+use App\Models\PermitApplication;
 
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 
 class Dashboard extends Controller
 {
-    public function index(){
-        
-        return view('dashboard.dashboard');
+    public function index()
+    {
+        $startOfMonth = Carbon::now()->startOfMonth();
+        $endOfMonth = Carbon::now()->endOfMonth();
+        $month = Carbon::now()->format('F');
+        $year = Carbon::now()->year;
+        $userId = auth()->user()->id;
+
+        $query = function($model,$startOfMonth,$endOfMonth,$userId){
+            try {
+                return $model::where('user_id', $userId)
+                    ->whereBetween('created_at', [$startOfMonth, $endOfMonth])
+                    ->count();
+            } catch (\Exception $e) {
+                Log::error('Error fetching applications: ' . $e->getMessage());
+                return 0;
+            }
+
+        };
+
+       $permitApplicationCount = $query(PermitApplication::class,$startOfMonth,$endOfMonth,$userId);
+       $foodestApplicationCount = $query(EstablishmentApplications::class,$startOfMonth,$endOfMonth,$userId);
+       $barbercosmApplicationCount = $query(BarbershopHairSalons::class,$startOfMonth,$endOfMonth,$userId);
+       $paymentCount = $query(Payments::class,$startOfMonth,$endOfMonth,$userId);
+
+       
+        return view('dashboard.dashboard', compact('permitApplicationCount','foodestApplicationCount','barbercosmApplicationCount','paymentCount','month','year'));
     }
 
-    public function fetchAppointments(){
-        $appointments = Appointments::with('applications')->where("appointment_date",date("Y-m-d"))
-        ->whereIn("facility_id",User::facilityUsers()->pluck('id')->flatten())->limit(10)->get();
-        //dd($appointments);
-        return view('dashboard.dashboard', compact('appointments'));
-    }
+   
     
 
     
--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -7,95 +7,144 @@
 
     <div class="main">
         @include('partials.navbar._navbar')
-          <main class="content px-3 py-4">
+        <main class="content px-3 py-4">
             <div class="container-fluid">
-                <div class="mb-3">
-                    <h3 class="fw-bold fs-4 mb-3">Dashboard</h3>
-                    <h4 class="fs-4 mb-3">Welcome {{ auth()->user()->firstname }} {{ auth()->user()->lastname }} </h4>
+                <div class="card shadow">
+                    <div>
+                        <div class="card-header">
+                            <h3 class="fw-bold fs-4">Dashboard</h3>
+                        </div>
 
-                    <div class="row">
-                        <div class="col-12 col-md-4 ">
-                            <div class="card">
-                                <div class="card-body py-4">
-                                    <h5 class="mb-2 fw-bold">
-                                        Onsite Applications
-                                    </h5>
-                                    <p class="mb-2 fw-bold">
+                        <h4 class="fs-4 py-3 px-3">Welcome {{ auth()->user()->firstname }} {{ auth()->user()->lastname }}
+                        </h4>
+
+                        <div class="card-body py-3">
+
+                            @if (in_array(auth()->user()->role_id, [1,3,10]))
+                                <div class="row">
+                                    <div class="col-12 col-md-4">
+                                        <div class="card">
+                                            <div class="card-body py-4">
+                                                <h5 class="mb-2 fw-bold">
+                                                    Food Permit Applications
+                                                </h5>
+                                                <p class="mb-2 fw-bold">
+                                                </p>
+                                                <div class="mb-0">
+                                                    <span class="badge text-success me-2">
+                                                        {{ $permitApplicationCount }}
+                                                    </span>
+                                                    <span class=" fw-bold">
+                                                        Since {{ $month }} {{ $year }}
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-12 col-md-4">
+                                        <div class="card">
+                                            <div class="card-body py-4">
+                                                <h5 class="mb-2 fw-bold">
+                                                    Food Establishment Applications
+                                                </h5>
+                                                <div class="mb-0">
+                                                    <span class="badge text-success me-2">
+                                                        {{ $foodestApplicationCount }}
+                                                    </span>
+                                                    <span class="fw-bold">
+                                                        Since {{ $month }} {{ $year }}
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-12 col-md-4 ">
+                                        <div class="card">
+                                            <div class="card-body py-4">
+                                                <h5 class="mb-2 fw-bold">
+                                                    Barber/Cosmetology
+                                                </h5>
+                                                <div class="mb-0">
+                                                    <span class="badge text-success me-2">
+                                                        {{ $barbercosmApplicationCount }}
+                                                    </span>
+                                                    <span class="fw-bold">
+                                                        Since {{ $month }} {{ $year }}
+                                                    </span>
+                                                </div>
+
+                                            </div>
+
+                                        </div>
 
 
-                                    </p>
-                                    <div class="mb-0">
-                                        <span class="badge text-success me-2">
-                                            +9.0%
-                                        </span>
-                                        <span class=" fw-bold">
-                                            Since Last Month
-                                        </span>
+
+                                    </div>
+
+                                </div>
+                            @endif
+
+                            @if (in_array(auth()->user()->role_id, [4]))
+                            <div class="row">
+                                <div class="col-12 col-md-4">
+                                    <div class="card">
+                                        <div class="card-body py-4">
+                                            <h5 class="mb-2 fw-bold">
+                                               Payments
+                                            </h5>
+                                            <p class="mb-2 fw-bold">
+                                            </p>
+                                            <div class="mb-0">
+                                                <span class="badge text-success me-2">
+                                                    {{ $paymentCount }}
+                                                </span>
+                                                <span class=" fw-bold">
+                                                    Since {{ $month }} {{ $year }}
+                                                </span>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+                           @endif
                         </div>
-                        <div class="col-12 col-md-4">
-                            <div class="card">
-                                <div class="card-body py-4">
-                                    <h5 class="mb-2 fw-bold">
-                                        Food Permit Applications
-                                    </h5>
-                                    <div class="mb-0">
-                                        <span class="badge text-success me-2">
-                                            +9.0%
-                                        </span>
-                                        <span class="fw-bold">
-                                            Since Last Month
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-12 col-md-4 ">
-                            <div class="card">
-                                <div class="card-body py-4">
-                                    <h5 class="mb-2 fw-bold">
-                                        Establishment Applications
-                                    </h5>
-                                    <div class="mb-0">
-                                        <span class="badge text-success me-2">
-                                            +9.0%
-                                        </span>
-                                        <span class="fw-bold">
-                                            Since Last Month
-                                        </span>
-                                    </div>
+                    </div>
+                </div>
+            </div>
 
 
-                                </div>
-                                <script>
-                                    const hamBurger = document.querySelector(".toggle-btn");
+    </div>
 
-                                    hamBurger.addEventListener("click", function() {
-                                        document.querySelector("#sidebar").classList.toggle("expand");
-                                    });
-                                </script>
+    </main>
 
-                                <script src="sweetalert2.all.min.js"></script>
+    </div>
 
-                                <script>
-                                    @if ($message = Session::get('success'))
-                                        Swal.fire({
-                                            title: "Success!",
-                                            text: "{{ $message }}",
-                                            icon: "success"
-                                        });
-                                    @endif
-                                
-                                    @if ($message = Session::get('error'))
-                                        Swal.fire({
-                                            title: "Error!",
-                                            text: "{{ $message }}",
-                                            icon: "error"
-                                        });
-                                    @endif
-                                </script>
-                                
+    <script>
+        const hamBurger = document.querySelector(".toggle-btn");
 
-                            @endsection
+        hamBurger.addEventListener("click", function() {
+            document.querySelector("#sidebar").classList.toggle("expand");
+        });
+    </script>
+
+    <script src="sweetalert2.all.min.js"></script>
+
+    <script>
+        @if ($message = Session::get('success'))
+            Swal.fire({
+                title: "Success!",
+                text: "{{ $message }}",
+                icon: "success"
+            });
+        @endif
+
+        @if ($message = Session::get('error'))
+            Swal.fire({
+                title: "Error!",
+                text: "{{ $message }}",
+                icon: "error"
+            });
+        @endif
+    </script>
+
+@endsection
