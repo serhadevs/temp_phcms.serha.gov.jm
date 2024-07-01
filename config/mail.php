78e9232a8dbe83da60503b962f7e44b2ce8fc03d
Tywayne Buchanan <tywaynebuchanan@gmail.com>
2024-07-01 17:34:08 -0500

    made changes to mail.php file

--- config/mail.php
+++ config/mail.php
@@ -114,4 +114,13 @@ return [
         ],
     ],
 
+    'stream' => [
+        'ssl' => [
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true,
+        ],
+    ],
+
+
 ];
