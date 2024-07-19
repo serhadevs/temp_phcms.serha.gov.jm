ff63692a8632e9ff800df64a281e1ce9cd243293
Tywayne Buchanan <tywaynebuchanan@gmail.com>
2024-07-19 10:21:44 -0500

    made changes to user settings

--- resources/views/partials/tables/users_table.blade.php
+++ resources/views/partials/tables/users_table.blade.php
@@ -14,7 +14,7 @@
         </div>
     </div>
     <div class="card-body">
-        <div class="row g-3 mt-2 mb-2">
+        <div class="row g-3">
             <table id="users" class="display table nowrap table-sm table-bordered" style="width:100%;max-width:100%">
                 <thead>
                     <tr>
@@ -72,22 +72,68 @@
             </table>
             
     </div>
-
-   
-
-    
 </div>
 <div class="card-footer">
      <a href="{{ route('dashboard.dashboard') }}" class="btn btn-danger">Back to Dashboard</a>   
 </div>
-<script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+{{-- <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
 <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script> --}}
 
+<script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.5.0/js/responsive.bootstrap5.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.bootstrap5.min.css">
+
+<script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.print.min.js"></script>
+<script src="https://cdn.datatables.net/plug-ins/1.13.7/api/sum().js"></script>
+{{-- 
  <script>
      new DataTable('#users', {
          responsive: true,
          scrollX:true,
      });
  </script>
+     --}}
+
+     <script>
+        new DataTable('#users', {
+            scrollX: true,
+            initComplete: function() {
+                loading.close()
+            },
+            dom: 'Bfrtip',
+            buttons: [
+                'copy', 'csv', 'excel', 'pdf', 'print'
+            ],
+            "order": [],
+            "footerCallback": function(row, data, start, end, display) {
+                var api = this.api(),
+                    data;
+            },
+            "aoColumnDefs": [{
+                "bSortable": false,
+                "aTargets": ["sorting_disabled"]
+            }],
+        });
+    </script>
+    
+    <script>
+        window.onload = () => {
+            buttons = document.querySelectorAll("div.dt-buttons button");
+            buttons.forEach((element) => {
+                element.classList.add("btn");
+                element.classList.add("btn-secondary")
+            })
+        }
+    </script>
     
