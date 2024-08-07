ae470653303ca498c5e0d985902b63e65313e985
Tywayne Buchanan <tywaynebuchanan@gmail.com>
2024-08-07 11:27:36 -0500

    make changes

--- app/Http/Controllers/Api/PermitApplicationApi.php
+++ app/Http/Controllers/Api/PermitApplicationApi.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\PermitApplication;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class PermitApplicationApi extends Controller
 {
@@ -13,6 +14,11 @@ class PermitApplicationApi extends Controller
         try {
             $applicant = PermitApplication::with('permitCategory', 'payment','establishmentClinics', 'signOffs', 'testResults', 'healthInterviews.healthInterviewSymptom.symptoms', 'appointment.editTransactions','messages')
                ->where('permit_no',$permit_no)->first();
+
+               $filePath = 'public/' . $applicant['permit_no'];
+
+               $fileContent = Storage::get($filePath);
+               $mimeType = Storage::mimeType($filePath);
 
             if (!$applicant) {
                 return response()->json(
@@ -26,7 +32,9 @@ class PermitApplicationApi extends Controller
                 "status" => "success",
                 "applicant" => $applicant,
                 // "token" => $token,
-            ]);
+            ],200);
+
+            
         } catch (\Exception $e) {
             return response()->json(['message' => 'An error occurred while fetching applications.', 'error' => $e->getMessage()], 500);
         }
