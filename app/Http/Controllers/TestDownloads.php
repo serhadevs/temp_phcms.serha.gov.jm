a3ece5762e06edc07aa9f232fb1ad4be611683f1
kiano-serha <kian.oconnor.serha@gmail.com>
2024-12-27 16:26:25 -0500

    update checked permit job

--- app/Http/Controllers/TestDownloads.php
+++ app/Http/Controllers/TestDownloads.php
@@ -48,14 +48,55 @@ class TestDownloads extends Controller
             $end_date = $request->route('date') . " 23:59:59";
         }
 
-        $downloads = Downloads::where('application_type_id', 1)
-            ->whereBetween('created_at', ["2024-04-08 12:00:00", "2024-04-08 23:59:59"])
-            // ->whereBetween('created_at', ['2024-04-08 12:00:00', '2024-04-08 23:59:59'])
-            ->get();
+        try {
+            $downloads = Downloads::where('application_type_id', 1)
+                ->whereBetween('created_at', ["2024-04-08 12:00:00", "2024-04-08 23:59:59"])
+                // ->whereBetween('created_at', ['2024-04-08 12:00:00', '2024-04-08 23:59:59'])
+                ->get();
 
-        dd($downloads);
+            foreach ($downloads as $download) {
+                $array = [];
+                $rand_string = "";
+                $file_name_separated = explode('-', explode('/', $download->download_url)[2]);
+                $facility_id = $file_name_separated[0];
+                $file_date = $file_name_separated[1] . '-' . $file_name_separated[2] . '-';
+                if (str_contains($download->download_url, '_')) {
+                    $file_date = $file_date . explode('_', $file_name_separated[3])[0];
+                    $rand_string = str_replace('.zip', '', explode('_', $file_name_separated[3])[1]);
+                } else {
+                    $file_date = $file_date . str_replace(".zip", '', $file_name_separated[3]);
+                }
+                $path = 'app/public/downloads/txts/' . $file_date .
+                    ($rand_string != "" ? "_" . $rand_string : '')
+                    . '/' . $facility_id . '/' . $facility_id . '-' .
+                    $file_date . '-Food_Handler_Permits.txt';
+                // $path = 'app/public/downloads/txts/2024-05-29_8787/KSA/KSA-2024-05-29-Food_Handler_Permits.txt';
+                $i = 0;
+                // $file = fopen(storage_path($path), 'r');
+                if (file_exists(storage_path($path)) && ($file = fopen(storage_path($path), 'r')) !== false) {
+                    while ($line = fgets($file)) {
+                        $array[$i] =  explode('.', explode("\t", $line)[8])[0];
+                        $i++;
+                    }
+                    fclose($file);
+                }
+                DB::beginTransaction();
+                foreach ($array as $permit_no) {
+                    $permit_id = PermitApplication::where('permit_no', $permit_no)->first()->id;
+                    ZippedApplications::where('application_id', $permit_id)
+                        ->where('application_type_id', 1)
+                        ->first()
+                        ->update(['written' => 1]);
+                }
+                DB::commit();
+            }
+            return "success job";
+        } catch (Exception $e) {
+            DB::rollBack();
+            return $e->getMessage();
+        }
         // CheckPermitZippedJobs::dispatch();
-        return "success";
+        // return "success";
     }
 
     public function writeAllFoodEstablishments()
