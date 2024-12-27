a4f527740107f364dd7f73023ebefc7861fd6f54
kiano-serha <kian.oconnor.serha@gmail.com>
2024-12-27 16:16:17 -0500

    update checked permit job

--- app/Models/Downloads.php
+++ app/Models/Downloads.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Downloads extends Model
 {
     use HasFactory;
-    use SoftDeletes;
+    // use SoftDeletes;
 
     protected $table = 'downloads';
 
