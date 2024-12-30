e731f3a6fd14644ad8ce938dfdafc6c85425638f
kiano-serha <kian.oconnor.serha@gmail.com>
2024-12-30 10:38:49 -0500

    checked job update week

--- app/Http/Controllers/TestDownloads.php
+++ app/Http/Controllers/TestDownloads.php
@@ -83,7 +83,7 @@ class TestDownloads extends Controller
                         $permit_id = $permit->id;
                         if ($zip = ZippedApplications::where('application_id', $permit_id)
                             ->where('application_type_id', 1)
-                            ->where('written', '<>', 1)
+                            ->where('written', NULL)
                             ->first()
                         ) {
                             $zip->update(['written' => 1]);
