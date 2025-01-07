2e29e9d5c90902af9708b0cfa397963119c44546
kiano-serha <kian.oconnor.serha@gmail.com>
2025-01-07 12:20:55 -0500

    clear job clarification

--- app/Http/Controllers/TestNewJobs.php
+++ app/Http/Controllers/TestNewJobs.php
@@ -92,7 +92,7 @@ class TestNewJobs extends Controller
             ->doesntHave('zippedApplication')
             ->has('payment')
             ->whereRelation('signOffs', 'created_at', '>', '2024-01-15')
-            ->whereRelation('signOffs', 'created_at', '<', '2024-09-15')
+            ->whereRelation('signOffs', 'created_at', '<', '2024-11-15')
             ->has('testResults')
             ->get();
         
