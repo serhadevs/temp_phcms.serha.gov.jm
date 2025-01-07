e13e2f0f21caca5f7de78d456cd1e76ddc97299d
kiano-serha <kian.oconnor.serha@gmail.com>
2025-01-07 11:13:19 -0500

    clear up jobs

--- app/Http/Controllers/TestNewJobs.php
+++ app/Http/Controllers/TestNewJobs.php
@@ -301,19 +301,13 @@ class TestNewJobs extends Controller
             $permits_affected = [];
             $i = 0;
             $unzipped_permits = ZippedApplications::where('application_type_id', 1)
-                // ->with('permitApplication')
-                // ->has('permitApplication')
-                // ->doesntHave('permitApplication')
+                ->with('permitApplication')
+                ->doesntHave('permitApplication')
                 ->where('created_at', '>', '2024-01-15')
                 ->where('created_at', '<', '2024-12-23 23:59:59')
                 ->where('written', NULL)
                 ->get();
-            // dd($unzipped_permits);
-
-            foreach($unzipped_permits as $permit){
-                $permits_affected[$i] = $permit->application_id;
-                $i++;
-            }
+            dd($unzipped_permits);
 
             dd($permits_affected);
 
