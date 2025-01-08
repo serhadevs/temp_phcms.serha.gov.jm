7b0e42e65c3e718a2f2cc8d793b2e43c3fc8f223
kiano-serha <kian.oconnor.serha@gmail.com>
2025-01-08 17:03:31 -0500

    check food est job

--- app/Http/Controllers/TestDownloads.php
+++ app/Http/Controllers/TestDownloads.php
@@ -161,7 +161,7 @@ class TestDownloads extends Controller
 
     public function checkFoodEstDownloads($id, $id2)
     {
-        $downloads = DB::table('downloads')->whereBetween('created_at', ['2024-04-30 18:47:45','2024-04-30 23:59:59'])
+        $downloads = DB::table('downloads')->whereBetween('created_at', [$id . ' 00:00:00', $id2 . ' 23:59:59'])
             ->where('application_type_id', 3)
             ->get();
 
