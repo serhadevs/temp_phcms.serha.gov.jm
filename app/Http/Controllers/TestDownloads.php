cc00946fa09aa6fee8d93031ebe8435d19f1edaa
kiano-serha <kian.oconnor.serha@gmail.com>
2025-01-13 16:07:13 -0500

    function to clean all establishment archives not downloaded

--- app/Http/Controllers/TestDownloads.php
+++ app/Http/Controllers/TestDownloads.php
@@ -211,6 +211,7 @@ class TestDownloads extends Controller
                 }
                 fclose($file);
             }
+            dd($array);
             DB::beginTransaction();
             foreach ($array as $permit_no) {
                 if ($establishment = EstablishmentApplications::where('permit_no', $permit_no)->first()) {
