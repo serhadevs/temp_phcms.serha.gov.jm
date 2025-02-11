32aba8ba6256fb32b73b61b27527f3d14abb846b
Tywayne Buchanan <tywaynebuchanan@gmail.com>
2025-02-10 19:01:06 -0500

    made changes to dashboard

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -156,10 +156,10 @@
                                   
                                 </div> --}}
 
-                                <div class="row g-3 mb-3">
+                                {{-- <div class="row g-3 mb-3">
                                    <livewire:establishmentnamesearch />
                                 </div>
-                               
+                                --}}
                                 <div class="row">
                                     <!-- Food Establishments that are expiring -->
                                     <div class="col mb-3">
