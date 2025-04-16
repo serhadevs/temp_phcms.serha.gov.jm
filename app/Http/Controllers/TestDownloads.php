7f47b833677fc50dede6e7bfdbd453cbe784a0dc
kiano-serha <kian.oconnor.serha@gmail.com>
2025-04-16 13:00:32 -0500

    fix food est job

--- app/Http/Controllers/TestDownloads.php
+++ app/Http/Controllers/TestDownloads.php
@@ -1545,7 +1545,7 @@ class TestDownloads extends Controller
                 }
                 fclose($file);
             }
-            dd($array);
+            // dd($array);
             DB::beginTransaction();
             foreach ($array as $permit_no) {
                 if ($establishment = EstablishmentApplications::where('permit_no', $permit_no)->first()) {
