67e45c34027343ba6bf6d05dff8f8e4f42dbb16b
kiano-serha <kian.oconnor.serha@gmail.com>
2025-05-19 21:33:56 -0500

    fix sign off controller for if user does not use last name filter

--- app/Http/Controllers/SignOffController.php
+++ app/Http/Controllers/SignOffController.php
@@ -70,7 +70,7 @@ class SignOffController extends Controller
 
         if ($app_type_id == 1) {
             if ($clinic_mode == "onsite") {
-                $start_last_name = $sign_off_params['filter_lastname'] == '1' ? $sign_off_params['start_last_name'] : '';
+                $start_last_name = $request->filter_lastname ? $sign_off_params['start_last_name'] : '';
                 $end_last_name = $sign_off_params['filter_lastname'] == '1' ? $sign_off_params['end_last_name'] : '';
                 $last_name_array = array([$start_last_name, $end_last_name]);
                 $applications = HealthInterview::with('permitApplication.permitCategory', 'permitApplication.establishmentClinics', 'permitApplication.testResults', 'permitApplication.travelHistory', 'healthInterviewSymptom.symptoms', 'permitApplication.payment')
