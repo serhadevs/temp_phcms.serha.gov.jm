554823832120c4f225bb7800701cf6d80499facd
Tywayne Buchanan <tywaynebuchanan@gmail.com>
2025-11-28 13:27:59 -0500

    made changes

--- resources/views/food_handlers_permit/partials/collectCard.blade.php
+++ resources/views/food_handlers_permit/partials/collectCard.blade.php
@@ -1,17 +1,7 @@
 <div class="card mt-3">
     <h5 class="card-header text-muted">Card Pickup Details</h5>
 
-    {{-- Card ready for pickup (not yet collected) --}}
-    @if ($collected_card && !$alreadyPickup)
-        <div class="card-footer">
-            <a href="{{ route('collectedcards.create', ['id' => $permit_application->id]) }}"
-               class="btn btn-success mt-1">
-                Enter Pickup Details
-            </a>
-        </div>
-    @endif
-
-    {{-- Card already picked up --}}
+    {{-- CASE 1: Card already picked up --}}
     @if ($alreadyPickup)
         <div class="card-footer">
             <p>
@@ -21,17 +11,26 @@
                 <strong>{{ \Carbon\Carbon::parse($pickup_details->created_at)->format('d F Y') }}</strong>.
             </p>
         </div>
-    @endif
 
-    {{-- Card expired --}}
-    @if ($card_expired)
+    {{-- CASE 2: Card expired and NOT collected --}}
+    @elseif ($card_expired)
         <div class="card-footer text-danger">
-            The card has expired.
+            The card has expired and can no longer be collected.
+        </div>
+
+    {{-- CASE 3: Card ready for pickup (printed + signed-off + not expired + not collected) --}}
+    @elseif ($collected_card && !$alreadyPickup && !$card_expired)
+        <div class="card-footer">
+            <a href="{{ route('collectedcards.create', ['id' => $permit_application->id]) }}"
+               class="btn btn-success mt-1">
+                Enter Pickup Details
+            </a>
+        </div>
+
+    {{-- CASE 4: Default fallback --}}
+    @else
+        <div class="card-footer text-muted">
+            Card is not printed or not signed off.
         </div>
     @endif
-
-   
-
-    
-
 </div>
