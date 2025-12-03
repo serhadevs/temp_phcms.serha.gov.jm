ee5cea6e332cbc40974c662d94630f6e76f99f24
Tywayne Buchanan <tywaynebuchanan@gmail.com>
2025-12-03 10:56:51 -0500

    made changes

--- app/Http/Controllers/PermitApplicationController.php
+++ app/Http/Controllers/PermitApplicationController.php
@@ -263,7 +263,7 @@ class PermitApplicationController extends Controller
         // Get pickup details
         $pickup_details = CollectedCards::where('app_id', $permit_application->id)->first();
 
-        return view('food_handlers_permit.view', compact('permit_application', 'appointments', 'appointment_available', 'edit_mode', 'categories', 'app_type_id', 'system_operation_type_id','card_expired',
+        return view('food_handlers_permit.view', compact('permit_application', 'appointments', 'appointment_available', 'edit_mode', 'categories', 'app_type_id', 'system_operation_type_id','card_expired','alreadyPickup'
             'pickup_details',
             'isAvailable'));
     }
