02c9b472b6422a1750d0b0dad1740a9ecc9c1502
Tywayne Buchanan <tywaynebuchanan@Tywaynes-MacBook-Pro.local>
2026-05-01 16:53:40 -0500

    made changes

--- resources/views/verify/certificate.blade.php
+++ resources/views/verify/certificate.blade.php
@@ -170,7 +170,7 @@
 
         <!-- WARNING -->
         <div class="text-center fw-bold border-top border-bottom py-2 my-4">
-            @if ($applicant->signOffs && $applicant->signOffs->is_granted)
+            @if ($applicant->signOffs)
                 THIS IS NOT A VALID FOOD HANDLERS PERMIT.
             @else
                 THIS IS AN OFFICAL FOOD HANDLERS E-CARD
