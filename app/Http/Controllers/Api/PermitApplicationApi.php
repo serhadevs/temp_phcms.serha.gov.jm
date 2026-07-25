b59b5c4693a849fa1f33c1fbf2ff444eb1d9af6d
Tywayne Buchanan <tywaynebuchanan@gmail.com>
2026-07-25 16:05:19 -0500

    made changes

--- app/Http/Controllers/Api/PermitApplicationApi.php
+++ app/Http/Controllers/Api/PermitApplicationApi.php
@@ -598,7 +598,7 @@ class PermitApplicationApi extends Controller
         ]);
 
         $onsite = EstablishmentClinics::with('permits','signOff')
-            ->where('name', $validated['company_name'])
+            ->where('id', $validated['application_number'])
             // ->where('sign_off_status',1)
             ->first();
 
